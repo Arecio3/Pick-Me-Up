b9f3ae39ec3c2347f2516be344b665eec25c7ffd
Arecio Canton <devmain@Arecios-iMac.fios-router.home>
2021-11-17 20:28:36 -0500

    Added logo to home screen

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import tw from 'twrnc';
+import logo from '../logo.png'
+const HomeScreen = () => {
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image 
+                style={{
+                    width: 100, 
+                    height: 100,
+                    resizeMode: 'contain'
+                }}
+                source={logo}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
