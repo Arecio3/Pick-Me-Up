43843b6fb8cab3ad2096a87ccf14571953adc897
Arecio Canton <devmain@Arecios-iMac.fios-router.home>
2021-11-19 12:48:27 -0500

    Working on Ride Options Page

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,16 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOG_MAPS_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
     return (
-        <SafeAreaView style={tw`flex-1 bg-gray-700`}>
-            <Text style={tw`text-gray-300`}>Yoooo</Text>
+        <SafeAreaView style={tw`flex-1 bg-gray-900`}>
+            <Text style={tw`text-center py-5 text-xl text-gray-400`}>Good Morning, Arecio</Text>
+            {/* Where to */}
+            <View style={tw`border-t border-gray-600 flex-shrink`}>
+                <View>
+                    <GooglePlacesAutocomplete 
+                    placeholder="Where to ?"
+                    debounce={400}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    styles={toInputBoxStyles}
+                    fetchDetails={true}
+                    enablePoweredByContainer={false}
+                    query={{
+                        key: GOOG_MAPS_KEY,
+                        language: "en"
+                    }}
+                    returnKeyType={"search"}
+                    minLength={2}
+                    onPress={(data, details = null) => {
+                        dispatch(setDestination({
+                            location: details.geometry.location,
+                            description: data.description,
+                        }))
+                        navigation.navigate("RideOptionsCard");
+                    }}
+                    />
+                </View>
+            </View>
         </SafeAreaView>
     );
 };
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        paddingTop: 20,
+        flex: 0
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 10,
+        fontSize: 18,
+        color: '#6B7280'
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    }
 })
