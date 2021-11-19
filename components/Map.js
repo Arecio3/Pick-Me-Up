0b09d6b1705c0f5d31f3a28639121a8eef408d92
Arecio Canton <devmain@Arecios-iMac.fios-router.home>
2021-11-19 13:16:51 -0500

    We got Directions

--- components/Map.js
+++ components/Map.js
@@ -1,50 +1,88 @@
-import * as React from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+import * as React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { useSelector } from "react-redux";
+import { selectOrigin, selectDestination } from "../slices/navSlice";
+import MapViewDirections from "react-native-maps-directions";
+import { GOOG_MAPS_KEY } from "@env";
+import { useRef, useEffect } from "react";
 
 const Map = () => {
-    // Grabs location
-    const origin = useSelector(selectOrigin);
+  // Grabs location
+  const origin = useSelector(selectOrigin);
+  const destination = useSelector(selectDestination);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (!origin || !destination) return;
+
+    // Zoom & Fit to markers
+    mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+      edgePadding: {top: 50, right: 50, bottom: 50, left: 50},
+    });
+
+    // Runs when either changes
+  }, [origin, destination]);
+
   return (
     <View style={styles.container}>
-      <MapView 
-      style={styles.map} 
-      initialRegion={{
-        latitude: origin.location.lat,
-        longitude: origin.location.lng,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-     }}
-     mapType="mutedStandard"
-    >
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        initialRegion={{
+          latitude: origin.location.lat,
+          longitude: origin.location.lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        mapType="mutedStandard"
+      >
+        {origin && destination && (
+          <MapViewDirections
+            origin={origin.description}
+            destination={destination.description}
+            apikey={GOOG_MAPS_KEY}
+            strokeWidth={3}
+            strokeColor="black"
+          />
+        )}
         {origin?.location && (
-            <Marker
+          <Marker
             coordinate={{
-                latitude: origin.location.lat,
-                longitude: origin.location.lng
+              latitude: origin.location.lat,
+              longitude: origin.location.lng,
             }}
             title={"Pick Up"}
             description={origin.description}
             identifier="origin"
-            />
+          />
         )}
-    </MapView>
+        {destination?.location && (
+          <Marker
+            coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+            }}
+            title={"Destination"}
+            description={destination.description}
+            identifier="destination"
+          />
+        )}
+      </MapView>
     </View>
   );
-}
+};
 
 export default Map;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
